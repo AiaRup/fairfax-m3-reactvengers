f7b1b9db910334c6dbbf401a33f4b565cd7a2fc5
LorenaBH <lorenabenitoherrero@gmail.com>
2019-05-10 12:11:49 +0200

    run build

---
+++ docs/build/precache-manifest.7eaf08a6b3ce67f9bb57d5de33594bd4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "862a8f048efe86ae14747ad098945c12",
+    "url": "./index.html"
+  },
+  {
+    "revision": "68619379c04e249856c4",
+    "url": "./static/css/main.da8c8d58.chunk.css"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "./static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "68619379c04e249856c4",
+    "url": "./static/js/main.52ec21a5.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "ad9d9032e9c4ca2faef51a95b91838d8",
+    "url": "./static/media/awesome-logo.ad9d9032.svg"
+  },
+  {
+    "revision": "630ebad541ba26cb2f0d999ea1f91de5",
+    "url": "./static/media/preview-image.630ebad5.jpg"
+  }
+]);

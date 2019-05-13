17dbbda72fb2703c114081650e90d568ea68036d
AiaRup <aiale88@gmail.com>
2019-05-13 11:21:27 +0200

    add prop types to iconLink

--- src/components/Card/PreviewCard/IconLink.js
+++ src/components/Card/PreviewCard/IconLink.js
@@ -2,21 +2,24 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const IconLink = ({ liClass, linkClass, iconClass, iconsData, hrefDefault, isVisible }) => {
-  const content = isVisible ?
-      <li className={`card__link-sm ${liClass}`}>
-        <a className={`${linkClass} social__link`} href={`${hrefDefault}${iconsData}`} target="_blank" rel="noopener noreferrer">
-            <i className={iconClass}></i>
-        </a>
-      </li>
-      :
-      '';
-    return content;
-}
+  const content = isVisible ? (
+    <li className={`card__link-sm ${liClass}`}>
+      <a className={`${linkClass} social__link`} href={`${hrefDefault}${iconsData}`} target="_blank" rel="noopener noreferrer">
+        <i className={iconClass} />
+      </a>
+    </li>
+  ) : (
+    ''
+  );
+  return content;
+};
 
 IconLink.propTypes = {
-    liClass: PropTypes.string.isRequired,
-    linkClass: PropTypes.string.isRequired,
-    iconClass: PropTypes.string.isRequired
-}
+  liClass: PropTypes.string.isRequired,
+  linkClass: PropTypes.string.isRequired,
+  iconClass: PropTypes.string.isRequired,
+  hrefDefault: PropTypes.string.isRequired,
+  isVisible: PropTypes.bool.isRequired
+};
 
 export default IconLink;

7e90e4d6a6cb79c743450b6d03f62edc2fb5e4dd
VictoriaDiro <victoriadiro01@gmail.com>
2019-05-13 15:07:10 +0200

    make class collapsible

--- src/components/Card/Collapsible.js
+++ src/components/Card/Collapsible.js
@@ -1,21 +1,17 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import TitleCollapsible from './TitleCollapsible';
 
-// destructuring props in the () of the function
-const Collapsible = ({ fieldsetClass, titleInfo, children, legendText }) => {
-  return (
-    <fieldset className={`form__fieldset ${fieldsetClass} fieldset__active`}>
+class Collapsible extends React.Component {
+  render () {
+    const {fieldsetClass, titleInfo, children, legendText} = this.props
+    return (
+      <fieldset className={`form__fieldset ${fieldsetClass} fieldset__active`}>
       <legend className="fieldset__legend">{legendText}</legend>
-      <TitleCollapsible titleInfo={titleInfo} />
-      {children}
+          <TitleCollapsible titleInfo={titleInfo}/>
+          {children}
     </fieldset>
-  );
-};
-
-Collapsible.propTypes = {
-  titleInfo: PropTypes.object.isRequired,
-  children: PropTypes.element.isRequired
-};
+    )
+  }
+}
 
 export default Collapsible;

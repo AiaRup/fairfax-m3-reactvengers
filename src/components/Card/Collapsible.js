f8074bd9ee531b3a11d2c6c1d29c4f431ea151f4
veroena <veronica.enamorado@gmail.com>
2019-05-14 11:31:18 +0200

    Add proptypes for collapsible compontent

--- src/components/Card/Collapsible.js
+++ src/components/Card/Collapsible.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import TitleCollapsible from './TitleCollapsible';
+import PropTypes from 'prop-types';
 
 class Collapsible extends React.Component {
 
   render () {
-    const {fieldsetClass, titleInfo, children, legendText, collapsibleObject,changeCollapsible} = this.props
+    const {fieldsetClass, titleInfo, children, legendText, collapsibleObject, changeCollapsible} = this.props
     // const contentChildren = collapsibleObject.isVisible ? {children} : '';
     return (
       <fieldset className={`form__fieldset ${fieldsetClass} fieldset__active`}>
@@ -14,6 +15,15 @@ class Collapsible extends React.Component {
     </fieldset>
     )
   }
+}
+
+Collapsible.propTypes = {
+  fieldsetClass: PropTypes.string,
+  titleInfo: PropTypes.object,
+  children: PropTypes.element,
+  legendText: PropTypes.string,
+  collapsibleObject: PropTypes.object,
+  changeCollapsible: PropTypes.func
 }
 
 export default Collapsible;
